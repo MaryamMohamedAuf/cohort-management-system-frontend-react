66323ed3a4bd0a5d2aa2ab8218aa5e398a7daf51
Maryam Mohamed Auf <maryammohamedauf@gmail.com>
2024-08-27 19:07:09 +0300

    Host the backend

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://oysmdvhkgb2n6gt5izlmc5wcnm0vrtez.lambda-url.us-east-1.on.aws/api',
     headers: {
         'Content-Type': 'application/json',
     }
